741d4334666163f92bdbf7544a9fe9fe4de3b825
Emilio <emilio.pg95@gmail.com>
2024-08-19 21:31:05 -0600

    🚧 chore: Fixing typos and adding middleware for parsing request bodies

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4001;
 // Add middleware for handling CORS requests from index.html
 app.use(cors());
 
-// Add middware for parsing request bodies here:
+// Add middleware for parsing request bodies here:
 app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
@@ -25,5 +25,5 @@ if (!module.parent) {
   // Add your code to start the server listening at PORT below:
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
-  })
+  });
 }
