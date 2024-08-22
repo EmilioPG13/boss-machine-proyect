8201269c613d9b7296134c06f5ec9fe271bcd488
Emilio <emilio.pg95@gmail.com>
2024-08-21 20:36:13 -0600

    🚧 feat: Add input validation for million dollar idea

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,18 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    const { numWeeks, weeklyRevenue } = req.body;
+
+    if (!numWeeks || !weeklyRevenue || isNaN(numWeeks) || isNaN(weeklyRevenue)) {
+        return res.status(400).send('Invalid input');
+    }
+    
+    const totalValue = numWeeks * weeklyRevenue;
+
+    if (totalValue < 1000000) {
+        return res.status(400).send('Idea is not worth at least one million dollars');
+    }
+
+    next();
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
